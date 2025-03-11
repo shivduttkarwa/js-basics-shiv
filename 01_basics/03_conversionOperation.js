0df225cd5fde5a09337af36435e8faa0a4940e68
shivduttkarwa <shivduttkarwa@gmail.com>
2025-03-11 08:42:56 +0000

    04_comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -8,7 +8,7 @@
 
 // "33" => 33
 // "33abc" => NaN
-// true => 1; false => 0
+// // true => 1; false => 0
 
 // let isLoggedIn = "shivdutt"
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
@@ -18,10 +18,33 @@
 // "" => false
 // "shivdutt" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
 
+// *********************** Operations ***********************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " shivdutt"
+// let str3 = str1 + str2
+// console.log(str3);
+//  console.log("1" + 2);
+//  console.log(1 + "2");
+//  console.log("1" + 2 + 2);
+//  console.log(1 + 2 + "2");
+ 
+
+ 
