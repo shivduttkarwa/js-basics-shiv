965e289a42e38916793bf7230dced755408bcc2b
shivduttkarwa <shivduttkarwa@gmail.com>
2025-03-12 12:10:22 +0000

    dates

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -18,27 +18,46 @@ const anotherId = Symbol('123');
 // Reference data types(Non-Premitive)
 // Object, Array, Function 
 
-const heroes = ["shaktiman", "naagraj", "doga"];
+// const heroes = ["shaktiman", "naagraj", "doga"];
 
-let myObj = {
-    name: "shivdutt karwa",
-    age: 38,
-}
+// let myObj = {
+//     name: "shivdutt karwa",
+//     age: 38,
+// }
 
-const myFunction = function(){
-    console.log("Hello world");
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
+// console.log(typeof bigNumber);
+// console.log(typeof outsideTemp);
+// console.log(typeof myFunction);
+// console.log(typeof myObj);
+// console.log(typeof heroes);
+// console.log(typeof score);
+// console.log(typeof scoreValue);
+// console.log(typeof isLoggedIn);
+// console.log(typeof userEmail);
+// console.log(typeof id);
+// console.log(typeof anotherId);
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Stack (Premitive), Heap (Non-Premitive)
+
+let myYoutubeName = "shivduttkarwa"
+let anotherName = myYoutubeName
+anotherName = "chaiaurcode"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
 }
-console.log(typeof bigNumber);
-console.log(typeof outsideTemp);
-console.log(typeof myFunction);
-console.log(typeof myObj);
-console.log(typeof heroes);
-console.log(typeof score);
-console.log(typeof scoreValue);
-console.log(typeof isLoggedIn);
-console.log(typeof userEmail);
-console.log(typeof id);
-console.log(typeof anotherId);
+let userTwo = userOne
+userTwo.email = "shivdutt@google.com"
+console.log(userOne.email);
 
 
 
