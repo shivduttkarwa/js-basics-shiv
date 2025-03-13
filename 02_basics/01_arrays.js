760bad68dcd4b9dbd9b96c592f2f8bb401f78dc1
shivduttkarwa <shivduttkarwa@gmail.com>
2025-03-13 03:33:07 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,4 +1,23 @@
 // arrays
 
 const myArr = [ 0,1,2,3,4,5]    
-console.log(myArr[1]);
+
+// const myHeroes = ["shaktiman", "naagraj", "doga"]
+
+// const myArr2 = new Array(1,2,3,4)
+//  console.log(myArr[1]);
+
+ //Array methods
+ 
+//  myArr.push(6)
+//  myArr.push(7)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+ const newArr = myArr.join()
+ console.log(myArr);
+ console.log(newArr);
+ 
